ef2ec7af50ab1a9c0a0f2a713165fa93771c6c36
ps-axjf <ps.axjf@gmail.com>
2011-04-13 17:51:28 +0300

    .

--- ckeditor/config.js
+++ ckeditor/config.js
@@ -5,7 +5,6 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 
 CKEDITOR.editorConfig = function( config )
 {
-       config.baseHref = 'https://github.com/ps-axjf/ckeditor-yaspeller/raw/gh-pages/ckeditor/';
        config.extraPlugins = 'yaspeller';
        config.removePlugins = 'scayt';
     //  config.corePlugins = 'core/ajax'
